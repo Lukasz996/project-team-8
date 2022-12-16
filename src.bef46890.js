08424bd5e2dcd717a9d5fa1283d2a300b17290cc
Lukasz996 <63703626+Lukasz996@users.noreply.github.com>
2022-12-16 18:39:35 +0000

    Deploying to gh-pages from @ Lukasz996/project-team-8@5bd704e160f44154905fd9ad76188741649bb74d 🚀

--- src.bef46890.js
+++ src.bef46890.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-team-8/src.e4e6f4bb.js.map
+//# sourceMappingURL=/project-team-8/src.bef46890.js.map
